406c8d6ad8406f971786c8b2a5961aad2ffbffd8
Ahad Islam <ahad.islam624@gmail.com>
2020-05-01 22:49:05 -0400

    learning how a binding property works

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,9 +8,41 @@
 
 import SwiftUI
 
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    // Property wrapper that allows a two-way data flow. when isOn is triggered, it sends that data to the "binded" object as well.
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    
+    var body: some View {
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors: offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct BindingContentView: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello World!")
     }
 }
 
