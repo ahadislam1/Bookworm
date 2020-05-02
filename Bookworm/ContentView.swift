f393edd9ad109f3254299f655ddb007cecb84090
Ahad Islam <ahad.islam624@gmail.com>
2020-05-02 14:07:57 -0400

    created a detail view, using stacks and showcasing all the model details

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -103,7 +103,7 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(books, id: \.self) { book in
-                    NavigationLink(destination: Text(book.title ?? "title")) {
+                    NavigationLink(destination: DetailView(book: book)) {
                         EmojiRatingView(rating: book.rating)
                             .font(.largeTitle)
                         
